6904b786e0698f8c34844ac8ce2bb4517831ab76
Ousmane MBALLO <ousmane.mballo@fahpo.com>
2021-03-08 17:50:26 +0000

    Creation de envoi repos et controller

--- src/main/java/com/simplon/transfer_rest_spring_angular/controller/EnvoiController.java
+++ src/main/java/com/simplon/transfer_rest_spring_angular/controller/EnvoiController.java
@@ -1,5 +1,85 @@
 package com.simplon.transfer_rest_spring_angular.controller;
 
+import com.simplon.transfer_rest_spring_angular.dao.EmeteurRepository;
+import com.simplon.transfer_rest_spring_angular.dao.EnvoieRepository;
+import com.simplon.transfer_rest_spring_angular.dao.RecepteurRepository;
+import com.simplon.transfer_rest_spring_angular.entities.Emeteur;
+import com.simplon.transfer_rest_spring_angular.entities.Envoie;
+import com.simplon.transfer_rest_spring_angular.entities.Recepteur;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
 public class EnvoiController {
+    @Autowired
+    EnvoieRepository envoieRepos;
+    @Autowired
+    RecepteurRepository recepteurRepos;
+    @Autowired
+    EmeteurRepository emeteurRepos;
+
+    @GetMapping("/envoie/all")
+    public List<Envoie> findAll(){return envoieRepos.findAll();}
+
+    @RequestMapping(value="/envoie/add", method = RequestMethod.POST)
+    public Envoie add(@RequestBody Envoie envoie){
+        Emeteur emeteur=emeteurRepos.save(envoie.getEmeteur());
+        Recepteur recepteur=recepteurRepos.save(envoie.getRecepteur());
+        envoie.setEmeteur(emeteur);
+        envoie.setRecepteur(recepteur);
+        return envoieRepos.save(envoie);
+    }
+
+    @RequestMapping(value = "/envoie/{id}", method = RequestMethod.GET)
+    public Envoie findById(@PathVariable int id){return envoieRepos.findById(id);}
+
+    @RequestMapping(value = "/envoie/emeteur/{id}", method = RequestMethod.GET)
+    public Envoie findByEmeteurId(@PathVariable int id){return envoieRepos.findByEmeteur_Id(id);}public Envoie findByEmeteurNci(String nci){return envoieRepos.findByEmeteur_Nci(nci);}
+
+    @RequestMapping(value = "/envoie/recepteur/{id}", method = RequestMethod.GET)
+    public Envoie findByRecepteurId(@PathVariable int id){return envoieRepos.findByRecepteur_Id(id);}
+
+    @RequestMapping(value = "/envoie/{nom}/{prenom}", method = RequestMethod.GET)
+    public Envoie findByRecepteurNomAndRecepteurPrenom(@PathVariable String nom, @PathVariable String prenom){return envoieRepos.findByRecepteur_NomAndRecepteur_Prenom(nom, prenom);}
+
+    @RequestMapping(value = "/envoie/recepteur/{tel}", method = RequestMethod.GET)
+    public Envoie findByRecepteurTel(@PathVariable String tel){return envoieRepos.findByRecepteur_Tel(tel);}
+
+    /*@RequestMapping(value="/envoie/update", method = RequestMethod.POST)*/
+    @PutMapping(value = "/envoie/update/{id}")
+    public Envoie update(@PathVariable int id, @RequestBody Envoie envoie){
+        try{
+            this.updateEmetteur(envoie.getEmeteur());
+            this.updateRecepteur(envoie.getRecepteur());
+            Envoie env=envoieRepos.findById(id);
+            env.setDate(envoie.getDate());
+            env.setMontant(envoie.getMontant());
+            envoieRepos.flush();
+            return env;
+        }catch (Exception e){
+            e.getStackTrace();
+        }
+        return null;
+    }
+
+    public void updateEmetteur(Emeteur emeteur){
+        Emeteur em=emeteurRepos.findById(emeteur.getId());
+        em.setTelephone(emeteur.getTelephone());
+        em.setPrenom(emeteur.getPrenom());
+        em.setNom(emeteur.getNom());
+        em.setNci(emeteur.getNci());
+        emeteurRepos.flush();
+    }
+
+    public void updateRecepteur(Recepteur recepteur){
+        Recepteur re=recepteurRepos.findById(recepteur.getId());
+        re.setNom(recepteur.getNom());
+        re.setPrenom(recepteur.getPrenom());
+        re.setTel(recepteur.getTel());
+        recepteurRepos.flush();
+    }
 
 }
--- src/main/java/com/simplon/transfer_rest_spring_angular/dao/EnvoieRepository.java
+++ src/main/java/com/simplon/transfer_rest_spring_angular/dao/EnvoieRepository.java
@@ -4,6 +4,16 @@ import com.simplon.transfer_rest_spring_angular.entities.Envoie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnvoieRepository extends JpaRepository<Envoie, Integer> {
+    Envoie findById(int id);
+    Envoie findByEmeteur_Id(int id);
+    Envoie findByEmeteur_Nci(String nci);
+    Envoie findByRecepteur_Id(int id);
+    Envoie findByRecepteur_NomAndRecepteur_Prenom(String nom, String prenom);
+    Envoie findByRecepteur_Tel(String tel);
+    //Envoie update(Envoie envoie);
 }
+
